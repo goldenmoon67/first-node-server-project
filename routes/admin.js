48bac7d1e57b7a6fa00336e4b5b7d85accf495be
goldenmoon67 <abdulmirac@gmail.com>
2023-01-30 01:44:54 +0300

    root dir utils

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const express= require("express");
 const path=require("path");
+const rootDir=require("../utils/path.js");
 
 const router=express.Router();
 
 
 router.get("/add-product",(req,res,next)=>{
-    res.sendFile(path.join(__dirname,"../","views","add-product.html"));// {__dirname} is location of this repo
+    res.sendFile(path.join(rootDir,"views","add-product.html"));// {rootDir} better than other way
 });
 
 router.post("/add-product",(req,res,next)=>{
