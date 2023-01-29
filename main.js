c7e06b7c64f0036d02ce7bab4df18a8a35e05ec9
goldenmoon67 <abdulmirac@gmail.com>
2023-01-30 00:34:18 +0300

    filters

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const shopRouter=require("./routes/shop");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(adminRouter);
+app.use("/admin",adminRouter);//now every url will get {/admin} before itselves
 app.use(shopRouter);
 app.use((req,res,next)=>{
-    res.status(404).send("<h1>Page Not Found<h1>");
+    res.status  (404).send("<h1>Page Not Found<h1>");
 })
 app.listen(3000);
