814c1049c16c9a736f10c365575582fe79764889
goldenmoon67 <abdulmirac@gmail.com>
2023-01-29 18:27:08 +0300

    go to new url

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const http= require("http");
 const express=require("express");
 
 const app=express();
-
-app.use((req,res,next)=>{
+app.use("/mirac",(req,res,next)=>{
+    console.log("in the another middleware Mirac")
+    res.send('<h1> Mirac Altinay </h1>')
+});
+app.use("/",(req,res,next)=>{
     console.log("in the another middleware")
     res.send('<h1> Hello from express </h1>')
 });
