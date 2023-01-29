c00f35c2b3b234b52b3a3c4c7fbdae997c48bee6
goldenmoon67 <abdulmirac@gmail.com>
2023-01-29 23:39:20 +0300

    post something and read the response and parse body

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const http= require("http");
+const bodyParser= require("body-parser");
 const express=require("express");
 
 const app=express();
-app.use("/mirac",(req,res,next)=>{
-    console.log("in the another middleware Mirac")
-    res.send('<h1> Mirac Altinay </h1>')
+app.use(bodyParser.urlencoded({extended:false}));//this is our parser method. it will parse the response's body
+app.use("/add-product",(req,res,next)=>{
+    res.send('<form action= "/product" method="POST"> <input type="text" name="title"> <button type="submit">Add product</button>  </form>');
+});
+app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
 });
 app.use("/",(req,res,next)=>{
-    console.log("in the another middleware")
     res.send('<h1> Hello from express </h1>')
 });
 app.listen(3000);
