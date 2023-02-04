85bba9590afaa82bfb497dbb394993d2cc38bfab
goldenmoon67 <abdulmirac@gmail.com>
2023-02-05 01:41:08 +0300

    working with ejs

--- main.js
+++ main.js
@@ -3,20 +3,12 @@ const express=require("express");
 const adminData= require("./routes/admin");
 const shopRouter=require("./routes/shop");
 const path = require("path");
-const expressHbs= require("express-handlebars");//pug was coming with express but handlebars not like that. thats why we should import it
 
 
 
 const app=express();
-app.engine(
-    'hbs',
-    expressHbs.engine({
-      layoutsDir: 'views/layouts/',//we have define layouts
-      defaultLayout: 'main-layout',//also we have to define default layout
-      extname: 'hbs'//extname should be aslo
-    })
-  );//now we added the handlebars to express as a engine
-app.set('view engine','hbs');//we added handlebars
+
+app.set('view engine','ejs');//ejs is coming with express 
 app.set('views','views');//this the where we created handlebars
 
 app.use(bodyParser.urlencoded({extended:false}));
