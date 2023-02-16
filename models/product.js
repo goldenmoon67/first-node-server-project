c5f61624b9062e4f55160f3240f9d3ae7dce5098
goldenmoon67 <abdulmirac@gmail.com>
2023-02-16 04:20:46 +0300

    some code beutify

--- models/product.js
+++ models/product.js
@@ -1,8 +1,16 @@
 const fs= require("fs");
 const path= require("path");
-
-const getProductsFromFile=(cb)=>{
-    //we will use it to make better our code
+const p=path.join(path.dirname(require.main.filename),'data','products.json');
+const getProductsFromFile=cb=>{
+  
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+           cb( []);
+        }else{
+            cb( JSON.parse(fileContent));
+        }
+       
+    });
 }
 
 module.exports = class Product{
@@ -11,26 +19,17 @@ module.exports = class Product{
     }
 
     save(){
-        const p=path.join(path.dirname(require.main.filename),'data','products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            let products=[];
-            if(!err){
-                products=JSON.parse(fileContent);
-            }
+
+        getProductsFromFile((products)=>{
             products.push(this);
             fs.writeFile(p,JSON.stringify(products),(err)=>{
                 console.log(err);
             })
         });
+        
     }
 
    static fetchAll(cb){//we are getting an error about length. this is coming from readFile code block. beacuse we use return inside of this code block. fetch all returns returns nothing
-    const p=path.join(path.dirname(require.main.filename),'data','products.json');
-    fs.readFile(p,(err,fileContent)=>{
-        if(err){
-           cb( []);
-        }
-       return cb( JSON.parse(fileContent));
-    });
+        getProductsFromFile(cb);
     }
 }
