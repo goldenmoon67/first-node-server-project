5fa83421da2a31eedc5fbcc6fa032c88259bcc2d
goldenmoon67 <abdulmirac@gmail.com>
2023-03-08 17:51:34 +0300

    getProducts

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,13 +1,14 @@
 const Product = require('../models/product');
 const Cart=require('../models/cart');
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows,dataField])=>{
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
+  }).catch(err=>console.log(err));
+ 
 };
 exports.getProduct = (req, res, next) => {
  const productId=req.params.productId;
