7a84561a0a4e2f2101b651924cccea0ed3f80203
goldenmoon67 <abdulmirac@gmail.com>
2023-04-10 01:34:57 +0300

    add user class and use realitions

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize=require("./util/database");
-
+const Product=require("./models/product");
+const User=require("./models/user");
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -20,7 +21,11 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-sequelize.sync().then((result) => {//this is for creating tables on database with using our models
+
+Product.belongsTo(User,{constraints:true, onDelete:"CASCADE"});//this is for realotion
+User.hasMany(Product);
+sequelize.sync({force:true})//this force is using to trigger the new changes on database we should not use it on prod version
+.then((result) => {
     //console.log(result);
     app.listen(3000);
 
